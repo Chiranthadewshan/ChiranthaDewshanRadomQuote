965269a5bdb3840984e6da0c2010498573003e83
Chirantha Dewshan <chiranthadewshan186@gmail.com>
2024-10-11 21:09:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-function load(){
+function load() {
     var x = new XMLHttpRequest();
 
-    x.onreadystatechange = function(){
-        if(x.status == 200 ){
-           var response= JSON.parse(x.responseText);
-            document.getElementById("quote").innerHTML = response.content +" ~ " + response.author;
-        }else if(x.readyState == 4){
-            document.getElementById("quote").innerHTML = "Error! , Please Try Again Later";
+    x.onreadystatechange = function() {
+        if (x.readyState == 4) { // Request is complete
+            if (x.status == 200) { // HTTP status is OK
+                var response = JSON.parse(x.responseText);
+                document.getElementById("quote").innerHTML = response.content + " ~ " + response.author;
+            } else { // HTTP status is not OK
+                document.getElementById("quote").innerHTML = "Error! Please Try Again Later";
+            }
         }
-    }
-    x.onerror = function(){
-        document.getElementById("quote").innerHTML = "Error! , Connection! issue";
-    }
+    };
 
-    x.open("GET","http://api.quotable.io/random",true);
+    x.onerror = function() { // Network errors or other issues
+        document.getElementById("quote").innerHTML = "Error! Connection issue";
+    };
+
+    x.open("GET", "http://api.quotable.io/random", true);
     x.send();
 }
